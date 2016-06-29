c91fbd61f1feeb110b7e6e5f7e088723ce3a22c1
h616016784 <wangyihanhuailong@163.com>
2016-06-29 11:46:15 +0800

    think in java

---
+++ src/com/thinkjava/MainStartClass.java
@@ -0,0 +1,12 @@
+package com.thinkjava;
+
+public class MainStartClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("think in java ³ÌÐò¿ªÊ¼£¡");
+	}
+
+}
